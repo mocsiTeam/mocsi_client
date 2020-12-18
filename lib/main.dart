5ee6e6df8071993071e670a96099a1a92043c94c
ivlup <ivlupsecond@gmail.com>
2020-12-18 21:12:15 +0200

    [MOCSI-42]: app; Packages configuration, analysis_options.yaml and injections

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:injectable/injectable.dart';
+import 'package:mocsi_client/presentation/injection.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  configureInjection(Environment.prod);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
