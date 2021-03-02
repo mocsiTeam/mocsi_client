f20156452c6f7dbbe71873023c2a95410cb4fe56
Ivan Taranov <37039674+ivatar39@users.noreply.github.com>
2021-03-02 11:27:19 +0200

    [MOCSI-64]: Added libraries - Added libraries for GraphQL - Checked compatibility between versions

--- lib/presentation/sign_in/sign_in_page.dart
+++ lib/presentation/sign_in/sign_in_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mocsi_client/application/auth/sign_in_form/sign_in_form_bloc.dart';
-import 'package:mocsi_client/presentation/core/colors.dart';
 import 'package:mocsi_client/presentation/sign_in/widgets/sign_in_form.dart';
 
 import '../injection.dart';
@@ -12,13 +11,7 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [secondaryDarkColor, secondaryColor],
-          ),
-        ),
+        decoration: const BoxDecoration(),
         child: BlocProvider(
           create: (context) => getIt<SignInFormBloc>(),
           child: SignInForm(),
